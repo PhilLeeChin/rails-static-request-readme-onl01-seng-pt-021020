1b72542ff028539ba87f9b199281c25835378528
Philip Lee-Chin <phil.leechin@outlook.com>
2020-06-13 01:33:28 +0000

    Automatically backed up by Learn

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,5 +1,6 @@
 class StaticController < ApplicationController
   def about
-    render "static/some_page"
+    render "static/some_page" #full view
+    # render "some_page" omit the folder
   end
 end
